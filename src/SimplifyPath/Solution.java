a908d74ccb7a21128a0f6611e425265e246eabf7
Anna <ml2003@list.ru>
2025-09-23 21:12:25 +0300

    #leetcode task71

---
+++ src/SimplifyPath/Solution.java
@@ -0,0 +1,31 @@
+package SimplifyPath;
+
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] components = path.split("/");
+
+        for (String component : components) {
+            if (component.equals("") || component.equals(".")) {
+                continue;
+            }
+
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(component);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+
+        return result.length() == 0 ? "/" : result.toString();
+    }
+}
